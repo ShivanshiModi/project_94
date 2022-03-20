16f956c66a0abe7099c5488a809beacf0bed9adb
ShivanshiModi <87198158+ShivanshiModi@users.noreply.github.com>
2022-03-20 09:46:03 +0530

    Update Kwitter.js

--- Kwitter.js
+++ Kwitter.js
@@ -2,5 +2,5 @@ function add_user()
 {
     username = document.getElementById("user_name").value;
     localStorage.setItem("user_name", username);
-    window.location = "kwitter_room.html";
+    window.location = "Kwitter_room.html";
 }
